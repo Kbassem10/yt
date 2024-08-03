f2b31daaa0c26e24dd2c40384d11ecbf2709224e
kbassem10 <kimobisso@gmail.com>
2024-08-03 16:04:04 +0300

    created the download script

---
+++ yt.py
@@ -0,0 +1,18 @@
+import yt_dlp as youtube_dl
+import tempfile
+import os
+
+def download_video(link):
+    try:
+        ydl_opts = {
+                    'outtmpl': '/mnt/d/Videos/Youtube_Downloads/%(title)s.%(ext)s',
+                    'format': 'best',
+                }
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([link])
+        print("Download completed")
+    except Exception as e:
+        print(f"Error: {e}")
+
+link = input("Link: ")
+download_video(link)
