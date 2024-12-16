817cc54f9ac12b3560f15b36039d90e86b6d2ea2
Karim Bassem <kimobisso@gmail.com>
2024-12-16 23:08:05 +0200

    added the option to save to a server

--- yt.py
+++ yt.py
@@ -1,12 +1,16 @@
 import yt_dlp
 import os
+import shutil
 
-def download_video(link, download_type):
+def download_video(link, download_type, save_dist):
+
+    save_dist_path = "/home/kbassem/Videos/Youtube_Downloads/%(title)s"
+
     try:
         if download_type == 1:  # Audio download
             ydl_opts = {
                 'format': 'bestaudio',
-                'outtmpl': '/home/kbassem/Videos/Youtube_Downloads/%(title)s',  # No extension here
+                'outtmpl': f'{save_dist_path}',
                 'postprocessors': [{
                     'key': 'FFmpegExtractAudio',
                     'preferredcodec': 'mp3',
@@ -15,7 +19,7 @@ def download_video(link, download_type):
             }
         elif download_type == 2:  # Video download
             ydl_opts = {
-                'outtmpl': '/home/kbassem/Videos/Youtube_Downloads/%(title)s.%(ext)s',
+                'outtmpl': f'{save_dist_path}',
                 'merge_output_format': 'mp4'
             }
 
@@ -25,6 +29,10 @@ def download_video(link, download_type):
 
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             ydl.download([link])
+        
+        '''if save_dist == 2:
+            server_path = "/run/user/1000/gvfs/smb-share:server=kb-imac.local,share=kb/Videos/Youtube_Download"
+            shutil.move(save_dist_path, server_path)'''
         print("Download completed")
 
     except Exception as e:
@@ -32,4 +40,5 @@ def download_video(link, download_type):
 
 link = input("Link: ")
 download_type = int(input("How Do You Want to Download it: \n 1. Audio\n 2. Video\n"))
-download_video(link, download_type)
+save_dist = int(input("Where to save the file: \n 1. Device\n 2. Server\n"))
+download_video(link, download_type, save_dist)
